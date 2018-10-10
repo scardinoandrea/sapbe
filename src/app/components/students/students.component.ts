421b8050310b6dae68007afebc0e185185250dea
andreascardino <andreascardino97@gmail.com>
2018-10-10 04:01:53 -0400

    css a scss

--- src/app/components/students/students.component.ts
+++ src/app/components/students/students.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrls: ['./students.component.css']
+  styleUrls: ['./students.component.scss']
 })
 export class StudentsComponent implements OnInit {
 
